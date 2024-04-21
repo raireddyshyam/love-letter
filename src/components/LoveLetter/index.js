0df4420daecd51bff1fb98bd48f8d18ca6b8ad98
raireddyshyam <raireddyshyam@gmail.com>
2024-04-21 13:52:55 +0530

    first

---
+++ src/components/LoveLetter/index.js
@@ -0,0 +1,47 @@
+import React, { useState, useRef } from 'react';
+import './index.css';
+import audioFile from './kushii.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        My dear kundanapu bomma🖤,<br />
+        With every beat of my heart, I find myself falling deeper in love with you. You are the one who makes my world brighter, my dreams sweeter, and my days more meaningful. Your love is a beautiful melody that plays in my heart.<br />i loveeee youuuu :)<br />
+        @Ur SweetHeart Shyam<br />
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
